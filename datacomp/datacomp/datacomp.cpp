c8e1cd5bd3c9a18d4f38ad60be35a8e7a07e2ac6
Tamjow <Tamjow@users.noreply.github.com>
2018-05-25 12:15:12 +0200

    fixed

--- datacomp/datacomp/datacomp.cpp
+++ datacomp/datacomp/datacomp.cpp
@@ -8,14 +8,15 @@
 using namespace std;
 
 const int dictsize = 9;
-string dictionary = "AAACAAAA";
-string text = "ABRACADABRADABRACAB";
-string textcpy = "ABRACADABRADABRACAB";
+string dictionary = "AAAAAAAA";
+string text = "ABABCDABCABCDCADABCA";
+string textcpy = text;
 string lookahead;
 vector <string> subsets;
 vector <string> compressed;
 
 void substrings(string str) {
+	subsets.erase(subsets.begin(), subsets.end());
 	for (int i = 0; i <= str.length(); i++) {
 		string s1 = str.substr(0, i); //get a substring of s from from the start to index i
 		subsets.push_back(s1);
@@ -27,26 +28,29 @@ void substrings(string str) {
 }
 
 void compress() {
-	lookahead = textcpy.substr(textcpy.length() - 4);
+	lookahead = textcpy.substr(0, 4);
 	substrings(lookahead);
-	string output="(";
+	string output = "(";
 	int maxlen = 0;
 	int maxidx = -1;
 	for (int i = 0; i < subsets.size(); i++) {
 		size_t index = dictionary.rfind(subsets[i]); //store index of found substring 
-		//cout << "loop" << endl;
+													 //cout << "loop" << endl;
 		if (dictionary.rfind(subsets[i]) != string::npos) { //if there is a match do this
-			//cout << "ifentered" << endl;
+															//cout << "ifentered" << endl;
 			if (subsets[i].length()>maxlen) { //execute if substring is the longest so far 
 				maxlen = subsets[i].length();
 				maxidx = index;
 			}
-			cout << "index of found: " << 7-index << " length of found: " << subsets[i].length() << endl;
-			
+			//cout << "index of found: " << 7 - index << " length of found: " << subsets[i].length() << endl;
+
 		}
 	}
-	
+
 	if (maxlen > 0) {//if match is found
+		cout << "dicc: " << dictionary << endl;
+		cout << "text: " << textcpy << endl;
+		cout << "lookahead: " << lookahead << endl;
 		maxidx = 7 - maxidx;//reverse index since indexing in example is right to left
 		output += "0,";
 		output += to_string(maxidx);
@@ -59,13 +63,18 @@ void compress() {
 		for (int i = 1;i <= maxlen;i++) {//shift the dictionary to the left by the length amount
 			for (int i = 1;i < dictionary.length();i++) {
 				dictionary[i - 1] = dictionary[i]; //since there's an empty space at the start shift all values left
-			}	
-			dictionary.erase(8-maxlen,maxlen);//remove as many ending characters as length of match
-			dictionary.append(lookahead,0,maxlen);
+			}
+			
 		}
+		dictionary.erase(8 - maxlen, maxlen);//remove as many ending characters as length of match
+		dictionary.append(lookahead, 0, maxlen);
+		textcpy.erase(0, maxlen);
 
 	}
 	else {
+		cout << "dicc: " << dictionary << endl;
+		cout << "text: " << textcpy << endl;
+		cout << "lookahead: " << lookahead << endl;
 		output += "1,";
 		output += subsets[0];
 		output += ")";
@@ -77,16 +86,20 @@ void compress() {
 		}
 		dictionary.erase(7);//remove last character
 		dictionary.append(lookahead, 0, 1); //add the new char
+		textcpy.erase(0, 1);
 	}
 }
 
 
 int main(void)
 {
-	
+
 	cout << "dicc: " << dictionary << endl;
-	compress();
-	cout << "dicc: " << dictionary << endl;
+	cout << "text: " << textcpy << endl;
+	cout << "lookahead: " << lookahead << endl;
+	while (textcpy.length() > 3) {
+		compress();
+	}
 
 	system("pause");
 	return 0;
